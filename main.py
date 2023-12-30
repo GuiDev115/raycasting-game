9b6a7e767a7afd14f2066e0b8b6d5e9a1df89547
guidev <guilherme.fabricio.115@gmail.com>
2023-12-29 22:30:59 -0300

    :wrench: Criando mapa, personagem, e suas covimentaoes com camera

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame as pg
 import sys
 from settings import *
+from map import *
+from player import *
 
 
 class Game:
@@ -8,17 +10,23 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.new_game()
 
     def new_game(self):
-        pass
+        self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
+        self.player.update()
         pg.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :1f}')
 
     def draw(self):
         self.screen.fill('black')
+        self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pg.event.get():
