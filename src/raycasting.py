b99fea8d79d7d8a2dd6119c8ac11cca52083aefb
guidev <guilherme.fabricio.115@gmail.com>
2024-03-27 00:58:24 -0300

    release: mode fps

--- src/raycasting.py
+++ src/raycasting.py
@@ -57,9 +57,17 @@ class RayCasting:
             else:
                 depth = depth_hor
                 
-            #debug
-            pg.draw.line(self.game.screen, 'darkgray', (ox * 100, oy * 100),
-                         (100 * ox + 100 * depth * cos_a, 100 * oy + 100 * depth * sin_a), 2)
+            #remove
+            depth *= math.cos(self.game.player.angle - ray_angle)
+            
+                
+            # projection
+            proj_height = SCREEN_DIST / (depth + 0.0001)
+            
+            #draw walls
+            color = [255 / (1 + depth ** 5 * 0.00002)] * 3
+            pg.draw.rect(self.game.screen, color, 
+                         (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
             
             ray_angle += DELTA_ANGLE
     
