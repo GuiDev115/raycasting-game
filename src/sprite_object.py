54ddde380ac8aa2763be55194cf7f8362e774995
guidev <guilherme.fabricio.115@gmail.com>
2024-05-12 23:49:28 -0300

    :bug: retirando o bug dos sprites animados

--- src/sprite_object.py
+++ src/sprite_object.py
@@ -53,7 +53,7 @@ class SpriteObject:
         self.get_sprite()
         
 class AnimatedSprite(SpriteObject):
-    def __init__(self, game, path='src/resources/sprites/animated_sprites/green_light/0.png',
+    def __init__(self, game, path='resources/sprites/animated_sprites/green_light/0.png',
                     pos=(11.5, 3.5), scale=0.8, shift=0.15, animation_time=120):
         self.animation_time = animation_time
         self.path = path.rsplit('/', 1)[0]
