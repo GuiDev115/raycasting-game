7aa80fd7003eeed1ee1e220aa83b11237c31bff1
guidev <guilherme.fabricio.115@gmail.com>
2024-05-14 00:57:16 -0300

    :package: Adicionando animacao de atirar na arma

--- src/weapon.py
+++ src/weapon.py
@@ -5,11 +5,26 @@ class Weapon(AnimatedSprite):
         super().__init__(game=game, path=path, scale=scale, animation_time=animation_time)
         self.image = deque(
             [pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale)) for img in self.images])
-        self.weapon_pos = (HALF_WIDTH - self.image[0].get_width() / 2, HEIGHT - self.image[0].get_height())
-        #self.weapon_pos = (WIDTH / 2 - self.image[0].get_width() / 2, HEIGHT / 2 - self.image[0].get_height() / 2)
+        self.weapon_pos = (HALF_WIDTH - self.images[0].get_width() // 2, HEIGHT - self.images[0].get_height())
+        self.reloading = False
+        self.num_images = len(self.images)
+        self.frame_counter = 0
+        self.damage = 50
+        
+    def animate_shot(self):
+        if self.reloading:
+            self.game.player.shot = False
+            if self.animation_trigger:
+               self.images.rotate(-1)
+               self.image = self.images[0]
+               self.frame_counter += 1
+               if self.frame_counter == self.num_images:
+                   self.reloading = False
+                   self.frame_counter = 0
         
     def draw(self):
-        self.game.screen.blit(self.image[0], self.weapon_pos)    
+        self.game.screen.blit(self.images[0], self.weapon_pos)    
         
     def update(self):
-        pass
+        self.check_animation_time()
+        self.animate_shot()
